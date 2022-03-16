9023c398e6f6167187e4b031e53c373625c3b157
Paul Sanders <psanders1@gmail.com>
2022-03-15 21:27:53 -0400

    Add tests

--- tests/test_tui.py
+++ tests/test_tui.py
@@ -1,15 +1,161 @@
-from weather_command._tui import _generate_title
+import io
+
+import pytest
+from httpx import AsyncClient
+from rich.console import Console
+from rich.panel import Panel
+
+from weather_command._config import LOCATION_BASE_URL
+from weather_command._tui import CurrentWeather, DailyWeather, HourlyWeather, _generate_title
+
+
+def render(panel: Panel) -> str:
+    console = Console(file=io.StringIO(), width=100)
+    console.print(panel)
+    return console.file.getvalue()  # type: ignore
 
 
 def test_generate_title_am_pm(mock_location):
     result = _generate_title(mock_location, True)
 
     assert mock_location.display_name in result
-    assert "am" or "pm" in result
+    assert "AM" or "PM" in result
 
 
 def test_generate_title_24_hour(mock_location):
     result = _generate_title(mock_location, False)
 
     assert mock_location.display_name in result
-    assert "am" and "pm" not in result
+    assert "AM" and "PM" not in result
+
+
+@pytest.mark.usefixtures("mock_cache_dir_with_file")
+async def test_current_weather_imperial(
+    mock_location_response, mock_location, mock_current_weather_response, monkeypatch
+):
+    async def mock_get_response(*args, **kwargs):
+        if LOCATION_BASE_URL in args[1]:
+            return mock_location_response
+
+        return mock_current_weather_response
+
+    monkeypatch.setattr(AsyncClient, "get", mock_get_response)
+    header = CurrentWeather(mock_location, "zip", "27455", "imperial", True)
+
+    await header.on_mount()
+    panel = header.render()
+    rendered = render(panel)
+
+    assert "Current Weather" in rendered
+    assert "(F)" in rendered
+    assert "AM" or "PM" in rendered
+
+
+@pytest.mark.usefixtures("mock_cache_dir_with_file")
+async def test_current_weather_metric(
+    mock_location_response, mock_location, mock_current_weather_response, monkeypatch
+):
+    async def mock_get_response(*args, **kwargs):
+        if LOCATION_BASE_URL in args[1]:
+            return mock_location_response
+
+        return mock_current_weather_response
+
+    monkeypatch.setattr(AsyncClient, "get", mock_get_response)
+    header = CurrentWeather(mock_location, "zip", "27455", "metric", False)
+
+    await header.on_mount()
+    panel = header.render()
+    rendered = render(panel)
+
+    assert "Current Weather" in rendered
+    assert "(C)" in rendered
+    assert "AM" and "PM" not in rendered
+
+
+@pytest.mark.usefixtures("mock_cache_dir_with_file")
+async def test_daily_weather_imperial(
+    mock_location_response, mock_location, mock_one_call_weather_response, monkeypatch
+):
+    async def mock_get_response(*args, **kwargs):
+        if LOCATION_BASE_URL in args[1]:
+            return mock_location_response
+
+        return mock_one_call_weather_response
+
+    monkeypatch.setattr(AsyncClient, "get", mock_get_response)
+    header = DailyWeather(mock_location, "zip", "27455", "imperial", True)
+
+    await header.on_mount()
+    panel = header.render()
+    rendered = render(panel)
+
+    assert "Daily Weather" in rendered
+    assert "(F)" in rendered
+    assert "AM" or "PM" in rendered
+
+
+@pytest.mark.usefixtures("mock_cache_dir_with_file")
+async def test_daily_weather_metric(
+    mock_location_response, mock_location, mock_one_call_weather_response, monkeypatch
+):
+    async def mock_get_response(*args, **kwargs):
+        if LOCATION_BASE_URL in args[1]:
+            return mock_location_response
+
+        return mock_one_call_weather_response
+
+    monkeypatch.setattr(AsyncClient, "get", mock_get_response)
+    header = DailyWeather(mock_location, "zip", "27455", "metric", False)
+
+    await header.on_mount()
+    panel = header.render()
+    rendered = render(panel)
+
+    assert "Daily Weather" in rendered
+    assert "(C)" in rendered
+    assert "AM" and "PM" not in rendered
+
+
+@pytest.mark.usefixtures("mock_cache_dir_with_file")
+async def test_hourly_weather_imperial(
+    mock_location_response, mock_location, mock_one_call_weather_response, monkeypatch
+):
+    async def mock_get_response(*args, **kwargs):
+        if LOCATION_BASE_URL in args[1]:
+            return mock_location_response
+
+        return mock_one_call_weather_response
+
+    monkeypatch.setattr(AsyncClient, "get", mock_get_response)
+    header = HourlyWeather(mock_location, "zip", "27455", "imperial", True)
+
+    await header.on_mount()
+    panel = header.render()
+    rendered = render(panel)
+
+    assert "Hourly Weather" in rendered
+    assert "(F)" in rendered
+    assert "AM" or "PM" in rendered
+
+
+@pytest.mark.usefixtures("mock_cache_dir_with_file")
+async def test_hourly_weather_metric(
+    mock_location_response, mock_location, mock_one_call_weather_response, monkeypatch
+):
+    async def mock_get_response(*args, **kwargs):
+        if LOCATION_BASE_URL in args[1]:
+            return mock_location_response
+
+        return mock_one_call_weather_response
+
+    monkeypatch.setattr(AsyncClient, "get", mock_get_response)
+    header = HourlyWeather(mock_location, "zip", "27455", "metric", False)
+
+    await header.on_mount()
+    panel = header.render()
+    rendered = render(panel)
+
+    assert "Hourly Weather" in rendered
+    assert "(C)" in rendered
+    assert "AM" and "PM" not in rendered
